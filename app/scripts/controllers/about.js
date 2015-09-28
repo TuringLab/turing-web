b1f86f3dd2cac5a6b86235c308f43d42fc59e280
Henry Miskin <henry.miskin@talktalk.net>
2015-09-28 14:00:47 +0100

    Inital commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name turingWebApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the turingWebApp
+ */
+angular.module('turingWebApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name turingWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the turingWebApp
+ */
+angular.module('turingWebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
