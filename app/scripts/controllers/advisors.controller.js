cb3b06b0c930a54aea84ae72c7b9f5554eccc87a
Henry Miskin <henry.miskin@talktalk.net>
2015-10-05 15:16:35 +0100

    Completed advisory board section

--- app/scripts/controllers/advisors.controller.js
+++ app/scripts/controllers/advisors.controller.js
@@ -7,30 +7,39 @@ angular.module('turingWeb').controller('AdvisorsController',['$scope',function($
 		{
             name: 'Professor William Knottenbelt',
             image: 'images/advisors/knottenbelt.jpg',
-            positions : [{
-                name: 'Director of Centre for Cryptocurrency and Research',
-                location: ' Imperial College London'
-            }],
+	        position: {
+	            heading: 'Imperial College London',
+	        	detail: 'Director of Centre for Cryptocurrency and Research'
+	        },
             homepage: 'http://www.doc.ic.ac.uk/~wjk/',
             linkedin: 'https://www.linkedin.com/in/williamknottenbelt',
             about: 'Will teaches a number of undergraduate and postgraduate courses at Imperial and supervises numerous student projects. These experiences make Will a very worthwhile advisor to give clear direction to TuringLab.'
+		},{            
+			name: 'Professor Susan Eisenbach',
+            image: 'images/advisors/eisenbach.jpg',
+	        position: {
+	            heading: 'Imperial College London',
+	        	detail: 'Head of the Department of Computing'
+	        },
+            homepage: 'http://www.imperial.ac.uk/people/s.eisenbach',
+            about: 'Susan has been part of Imperial College since 1983, Her previous roles include Dean of Learning & Teaching and Director of studies in the Department of Computing. Susan supports the work of the TuringLab team in the Department of Computing and imparts her experience as a teacher and educator.'
 		},{
             name: 'Dr Anandha Gopalan',
             image: 'images/advisors/gopalan.jpg',
-            positions : [{
-                name: 'Teaching Fellow in the Department of Computing',
-                location: ' Imperial College London'
-            }],
+	        position: {
+	            heading: 'Imperial College London',
+	        	detail: 'Teaching Fellow in the Department of Computing'
+	        },
             homepage: 'http://www.doc.ic.ac.uk/~axgopala/',
             linkedin: 'https://www.linkedin.com/pub/anandha-gopalan/83/419/a25',
             about: 'Anandha has a passion for teaching computing, hoping to someday bring this key skill to many students through the running of numerous projects each year aimed to bring technology to the un-technological. Anandha shares his interest and passion with the team.'
 		},{
             name: 'Dr Manolis Mavrikis',
             image: 'images/advisors/mavrikis.jpg',
-            positions : [{
-                name: 'Researcher in Learning Technologies at the London Knowledge Lab',
-                location: ' University College London'
-            }],
+	        position: {
+	            heading: 'University College London',
+	        	detail: 'Researcher at the London Knowledge Lab'
+	        },
             homepage: 'http://www.lkl.ac.uk/manolis/',
             linkedin: 'https://www.linkedin.com/pub/manolis-mavrikis/4/57/14b',
             about: 'Manolis research interests lie at the intersection of technology-enhanced learning, human-computer interaction and artificial intelligence. Along with other members of the London Knowledge Lab the input from Manolis ensures the TuringLab sessions are from the correct Pedagogical approach.'
--- app/scripts/controllers/team.controller.js
+++ app/scripts/controllers/team.controller.js
@@ -10,63 +10,49 @@ angular.module('turingWeb').controller('TeamController',['$scope',function($scop
             image: 'images/team/henrym.jpg',
             about: 'Henry wants to share his passion for problem solving in engineering and computing with upcoming inventors.',
             positions : [{
-                name: 'MSc Computer Science',
-                location: ' Imperial College London'
+                heading: 'MSc Computer Science',
+                detail: ' Imperial College London'
             },{
-                name: 'BEng Mechanical Engineering',
-                location: 'University of Southampton'
+                heading: 'BEng Mechanical Engineering',
+                detail: 'University of Southampton'
             }],
             linkedin: 'https://uk.linkedin.com/in/hpgmiskin'
-            // email: 'mailto:hpgmiskin@turinglab.co.uk'
-        // },{
-        //     name: 'Tom Murray',
-        //     image: 'images/team/tom.jpg',
-        //     positions : [{
-        //         name: 'MSc Computer Science',
-        //         location: ' Imperial College London'
-        //     },{
-        //         name: 'MSc Applied Mathematics',
-        //         location: "Queen's University Belfast"
-        //     }],
         },{
             name: 'Sam Green',
             about: 'Sam looks forward to passing on an enthusiasm for design and technology to a new generation of innovators.',
             image: 'images/team/sam.jpg',
             positions : [{
-                name: 'MSc Computer Science',
-                location: ' Imperial College London'
+                heading: 'MSc Computer Science',
+                detail: ' Imperial College London'
             },{
-                name: 'BA Architecture',
-                location: 'University of Cambridge'
+                heading: 'BA Architecture',
+                detail: 'University of Cambridge'
             }],
             linkedin: 'https://uk.linkedin.com/in/greensamuel'
-            // email: 'mailto:sam@turinglab.co.uk'
         },{
             name: 'Priyav Shah',
             about: 'Priyav is driven by getting kids excited about the power of technology.',
             image: 'images/team/priyav.jpg',
             positions : [{
-                name: 'MSc Computer Science',
-                location: ' Imperial College London'
+                heading: 'MSc Computer Science',
+                detail: ' Imperial College London'
             },{
-                name: 'BA Psychology',
-                location: 'University of York'
+                heading: 'BA Psychology',
+                detail: 'University of York'
             }],
             linkedin: 'https://www.linkedin.com/in/priyavshah91'
-            // email: 'mailto:priyav@turinglab.co.uk'
         },{
             name: 'Henry Lake',
             about: 'Henry enjoys bringing games to life through the application of physics.',
             image: 'images/team/henryl.jpg',
             positions : [{
-                name: 'MSc Computer Science',
-                location: ' Imperial College London'
+                heading: 'MSc Computer Science',
+                detail: ' Imperial College London'
             },{
-                name: 'MEng Physics',
-                location: 'Imperial College London'
+                heading: 'MEng Physics',
+                detail: 'Imperial College London'
             }],
             linkedin: '#'
-            // email: 'mailto:henry@turinglab.co.uk'
         }
     ];
 
