eeceb37b7de7be48a67b4cb4f572e73526e4b213
Henry Miskin <henry.miskin@talktalk.net>
2015-10-07 08:41:33 +0100

    Modified advisor information

--- app/scripts/controllers/advisors.controller.js
+++ app/scripts/controllers/advisors.controller.js
@@ -8,41 +8,45 @@ angular.module('turingWeb').controller('AdvisorsController',['$scope',function($
             name: 'Professor William Knottenbelt',
             image: 'images/advisors/knottenbelt.jpg',
 	        position: {
-	            heading: 'Imperial College London',
-	        	detail: 'Director of Centre for Cryptocurrency and Research'
+	        	role: 'Director of Industrial Liaison',
+	        	department: 'Department of Computing',
+	          university: 'Imperial College London'
 	        },
             homepage: 'http://www.doc.ic.ac.uk/~wjk/',
             linkedin: 'https://www.linkedin.com/in/williamknottenbelt',
-            about: 'Will teaches a number of undergraduate and postgraduate courses at Imperial and supervises numerous student projects. These experiences make Will a very worthwhile advisor to give clear direction to TuringLab.'
+            about: 'Will has been teaching Computer Science for over 15 years and has received numerous awards honoring the excellency of the undergraduate and postgraduate courses he runs.'
 		},{            
 			name: 'Professor Susan Eisenbach',
             image: 'images/advisors/eisenbach.jpg',
 	        position: {
-	            heading: 'Imperial College London',
-	        	detail: 'Head of the Department of Computing'
+	        	role: 'Head of Department',
+	        	department: 'Department of Computing',
+	          university: 'Imperial College London'
 	        },
             homepage: 'http://www.imperial.ac.uk/people/s.eisenbach',
-            about: 'Susan has been part of Imperial College since 1983, Her previous roles include Dean of Learning & Teaching and Director of studies in the Department of Computing. Susan supports the work of the TuringLab team in the Department of Computing and imparts her experience as a teacher and educator.'
+            about: 'Susan has been at Imperial College since 1983. Susan has a rich history teaching computing to a wide range of students from different age groups, abilities and backgrounds.'
 		},{
             name: 'Dr Anandha Gopalan',
             image: 'images/advisors/gopalan.jpg',
 	        position: {
-	            heading: 'Imperial College London',
-	        	detail: 'Teaching Fellow in the Department of Computing'
+	        	role: 'Teaching Fellow',
+	        	department: 'Department of Computing',
+	          university: 'Imperial College London'
 	        },
             homepage: 'http://www.doc.ic.ac.uk/~axgopala/',
             linkedin: 'https://www.linkedin.com/pub/anandha-gopalan/83/419/a25',
-            about: 'Anandha has a passion for teaching computing, hoping to someday bring this key skill to many students through the running of numerous projects each year aimed to bring technology to the un-technological. Anandha shares his interest and passion with the team.'
+            about: 'Anandha manages the teaching of computing to several hundred students at Imperial College. In addition he regularly supervises projects which develop teaching technology.'
 		},{
             name: 'Dr Manolis Mavrikis',
             image: 'images/advisors/mavrikis.jpg',
 	        position: {
-	            heading: 'University College London',
-	        	detail: 'Researcher at the London Knowledge Lab'
+	        	role: 'Senior Research Fellow',
+	        	department: ' London Knowledge Lab',
+	          university: 'University College Lonon',
 	        },
             homepage: 'http://www.lkl.ac.uk/manolis/',
             linkedin: 'https://www.linkedin.com/pub/manolis-mavrikis/4/57/14b',
-            about: 'Manolis research interests lie at the intersection of technology-enhanced learning, human-computer interaction and artificial intelligence. Along with other members of the London Knowledge Lab the input from Manolis ensures the TuringLab sessions are from the correct Pedagogical approach.'
+            about: 'Manolis is an expert on Technology-Enhanced Learning. He has developed a number of technology products that teach STEM subjects to children and currently trains teachers use relevant learning technologies in education.'
 		}
 	];
 
