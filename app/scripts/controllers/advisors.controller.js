18c0269da5385d97350c5a4e9a81da4a964413e7
Henry Miskin <henry.miskin@talktalk.net>
2015-10-08 19:04:35 +0100

    Added Rose Luckin to advisor board

--- app/scripts/controllers/advisors.controller.js
+++ app/scripts/controllers/advisors.controller.js
@@ -2,9 +2,29 @@
 
 angular.module('turingWeb').controller('AdvisorsController',['$scope',function($scope){
 
+	$scope.getStyle = function(index){
+		if (index === 3){
+			return 'col-xs-offset-1 col-md-offset-2 col-sm-offset-0';
+		}
+		if (index === 4){
+			return 'col-xs-offset-1 col-md-offset-0 col-sm-offset-3';
+		}
+		return 'col-xs-offset-1 col-md-offset-0 col-sm-offset-0';
+	}
+
 	$scope.heading = 'Advisors';
 	$scope.advisors = [
 		{
+			name: 'Professor Susan Eisenbach',
+            image: 'images/advisors/eisenbach.jpg',
+	        position: {
+	        	role: 'Head of Department',
+	        	department: 'Department of Computing',
+	          university: 'Imperial College London'
+	        },
+            homepage: 'http://www.imperial.ac.uk/people/s.eisenbach',
+            about: 'Susan has been at Imperial College since 1983. Susan has a rich history teaching computing to a wide range of students from different age groups, abilities and backgrounds.'
+		},{
             name: 'Professor William Knottenbelt',
             image: 'images/advisors/knottenbelt.jpg',
 	        position: {
@@ -16,15 +36,16 @@ angular.module('turingWeb').controller('AdvisorsController',['$scope',function($
             linkedin: 'https://www.linkedin.com/in/williamknottenbelt',
             about: 'Will has been teaching Computer Science for over 15 years and has received numerous awards honoring the excellency of the undergraduate and postgraduate courses he runs.'
 		},{            
-			name: 'Professor Susan Eisenbach',
-            image: 'images/advisors/eisenbach.jpg',
+				name: 'Professor Rose Luckin',
+            image: 'images/advisors/luckin.jpg',
 	        position: {
-	        	role: 'Head of Department',
-	        	department: 'Department of Computing',
-	          university: 'Imperial College London'
+	        	role: 'Chair',
+	        	department: 'London Knowledge Lab',
+	          university: 'University College London'
 	        },
-            homepage: 'http://www.imperial.ac.uk/people/s.eisenbach',
-            about: 'Susan has been at Imperial College since 1983. Susan has a rich history teaching computing to a wide range of students from different age groups, abilities and backgrounds.'
+            homepage: 'http://www.lkl.ac.uk/cms/index.php?option=com_comprofiler&task=userProfile&user=128',
+            linkedin: 'https://www.linkedin.com/pub/rose-luckin/3/500/524',
+            about: 'Rose is chair of learning with digital technologies at UCL. She is also Professor of Learner Centered Design with extensive expertise in participatory methods to the development and evaluation of technology for learning.'
 		},{
             name: 'Dr Anandha Gopalan',
             image: 'images/advisors/gopalan.jpg',
